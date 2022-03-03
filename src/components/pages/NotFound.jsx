dd7f0016e03d9306db07227bfa0310b56d3b9d36
Natalia Marin Gallego <0502.nataliia@gmail.com>
2022-03-02 22:55:38 -0500

    Create and router NotFound page

---
+++ src/components/pages/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DefaulLayout from "../layouts/DefaultLayout";
+/**
+ * Not Found Compnent
+ *@returns {JSX.Element}
+ */
+
+ export const NotFound = () => (
+     <DefaulLayout>
+         <h1>Not Found Page</h1>
+     </DefaulLayout>
+ );
+
+ export default NotFound;
