24d17cd583422b9e084c04f6e84c543024806747
Natalia Marin Gallego <0502.nataliia@gmail.com>
2022-03-12 03:17:51 -0500

    Define nav brand style

--- src/components/molecules/nav-brand/NavBrand.jsx
+++ src/components/molecules/nav-brand/NavBrand.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import Logo from 'assets/img/logo.png';
+import './NavBrand.css';
 
 /**
  * Navigation brand component
@@ -12,9 +13,9 @@ const NavBrand = () => {
         navigate('/');
     };
     return (
-        <div onClick={handleClick}>
-            <img src={Logo} alt='Logo de la aplicación' />
-            <span>POKEDEX_</span>
+        <div className="m-nav-brand" onClick={handleClick}>
+            <img className="m-nav-brand__image" src={Logo} alt='Logo de la aplicación' />
+            <span className="m-nav-brand__text">POKEDEX_</span>
         </div>
     );
 };
