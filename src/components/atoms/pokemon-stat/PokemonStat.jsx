71c467426c9532f81e4ae49c2f1b3ac03d09404d
Natalia Marin Gallego <0502.nataliia@gmail.com>
2022-04-11 01:16:17 -0500

    Define style for PokemonStat component

--- src/components/atoms/pokemon-stat/PokemonStat.jsx
+++ src/components/atoms/pokemon-stat/PokemonStat.jsx
@@ -5,11 +5,14 @@ import './PokemonStat.css';
  * Pokemon stat content
  * @returns {JSX.Element}
  */
-const PokemonStat = () => (
+const PokemonStat = (props) => {
+    const {name, value} = props;
+    return (
     <div className='a-pokemon-stat'>
-        <span className='a-pokemon-stat__name'>Nombre</span>
-        <span className='a-pokemon-stat__value'>Valor</span>
+        <span className='a-pokemon-stat__name'>{name}</span>
+        <span className='a-pokemon-stat__value'>{value}</span>
     </div>
-);
+ );
+};
 
 export default PokemonStat;
